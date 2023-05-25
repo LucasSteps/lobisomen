33e415f0dcdb694829679b46ac75d3e78a3f2d40
BrunoDeBrito <brunordebrito@gmail.com>
2023-05-24 21:01:12 -0300

    Alteração auth

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,30 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/**
+ *  TODO - ROTA DE LOGIN ARRUMAR DEPOIS
+ ** ROTA ALTERNATIVA MELHORAR DEPOIS
+ */
+Route::post('login', function (Request $request) {
+
+    if (Auth::attempt($request->only('email', 'password'))) {
+
+        $user = Auth::user();
+        $token = $user->createToken('app')->plainTextToken;
+
+        return response()->json($token, 200);
+
+    }
+
+    return response()->json([
+        'message' => 'Invalid login details'
+    ], 401);
+
 });
